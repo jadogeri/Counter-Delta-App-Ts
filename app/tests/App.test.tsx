e2ac5967b43686e2c10fbaadf07b653cbe8d3460
joseph adogeri <70236136+jadogeri@users.noreply.github.com>
2025-09-03 01:58:53 -0500

    REmoved dummy tests

--- app/tests/App.test.tsx
+++
@@ -1,9 +0,0 @@
-import { render } from '@testing-library/react-native';
-import MyComponent from './MyComponent';
-
-describe('MyComponent', () => {
-  test('renders the correct message', () => {
-    const { getByText } = render(<MyComponent message="Hello Expo!" />);
-    expect(getByText('Hello Expo!')).toBeDefined();
-  });
-});
